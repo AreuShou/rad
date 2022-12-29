7391f160862e34a87d0456208970fc7cfc8bfd05
AreuShou <palouarlette3@gmail.com>
2022-12-29 19:02:38 +0100

    employee

--- Views/EmployeesViews.Designer.vb
+++ Views/EmployeesViews.Designer.vb
@@ -30,9 +30,9 @@ Partial Class EmployeesViews
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(41, 22)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(30, 13)
+        Me.Label1.Size = New System.Drawing.Size(72, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "testa"
+        Me.Label1.Text = "EMPLOYEES"
         '
         'EmployeesViews
         '
